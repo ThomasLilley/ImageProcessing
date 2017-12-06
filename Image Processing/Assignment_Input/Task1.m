265b6a68fd763b42ab67062f9b48b51fb9241676
ThomasLilley <Thomas.lilley03@gmail.com>
2017-12-06 02:40:31 +0000

    Task 1 refactor

--- Image Processing/Assignment_Input/Task1.m
+++ Image Processing/Assignment_Input/Task1.m
@@ -1,5 +1,3 @@
-% MATLAB script for Assessment Item-1
-% Task-1
 clear; close all; clc;
 
 % Step-1: Load input image
@@ -14,41 +12,33 @@ figure;
 imshow(A);
 title('Step-2: Conversion of input image to greyscale');
 % find the size of the input image
-[M,N] = size(A);
+% Set the size for the new image
+NewM = 1668;
+NewN = 1836;
+O = zeros(NewM,NewN);
+% find the value to scale the image by
+ScaleM = NewM/(size(A,1));
+ScaleN = NewN/(size(A,2));
 
-% Step-3: Resize image from 556x612 to 1668x1836 (1:3)
-% using Nearest Neighbour Interpolation
-% The size of the output image
-MNewSize = 1668;
-NNewSize = 1836;
-% Calculate the scale the input image is being increased by dividing the
-% output image dimensions by the input image simensions
-XScale = MNewSize./(M-1);
-YScale = NNewSize./(N-1);
-
-% populates the image with zeros
-Out = zeros(MNewSize,NNewSize);
-
-%Interpolation stage, For the dimensions of the output 
-%image do the following
-for i = 0:MNewSize-1
- 
- for j = 0:NNewSize-1
- 
-    Out(i+1,j+1) = A(1+round(i./XScale),1+round(j./YScale));
- 
- end
+for i = 1:NewM
+    for j = 1:NewN
+        x = 1+round(i/ScaleM);
+        y = 1+round(j/ScaleN);
+        
+        if x ==557
+           x = 556;
+        end
+        if y == 613
+           y = 612;
+        end
+        
+        O(i,j) = A(x,y);
+        
+    end
 end
 
-%outputs the image as a figure in grayscale with a title
-Out = uint8(Out);
+O = uint8(O);
 figure(3);
-imshow(Out);
+imshow(O);
 title('Step-3: Nearest Neighbour Interpolation');
-
-
-% Step-4: Resize image from 556x612 to 1668x1836 (1:3)
-% using Bilinear interpolation
-
-
 
