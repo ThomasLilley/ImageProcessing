e4fa14627e3c6d56ff0da165413e21bcecc68c45
ThomasLilley <Thomas.lilley03@gmail.com>
2017-12-06 06:16:00 +0000

    Task 4

--- Image Processing/Assignment_Input/Task4.m
+++ Image Processing/Assignment_Input/Task4.m
@@ -5,15 +5,45 @@ clear; close all; clc;
 I = imread('Starfish.jpg');
 I = rgb2gray(I);
 
-mask = zeros(size(I));
-mask(25:end-25,25:end-25) = 1;
-figure
-imshow(mask)
-title('Initial Contour Location')
 
-T = activecontour(I,mask,400);
-%T = imbinarize(I, .9);
+mask = zeros(5);
+for c = 1:size(I,1)
+    for r = 1:size(I,2)
+        for i = 1:5
+            for j = 1:5
+                if c < 350 && r < 430            
+                mask(i,j) = I(c+i,r+j);
+                end                
+            end
+        end
+        M = median(mask(:));        
+        I(c,r) = M;              
+    end
+end
+
+figure(1);
+imshow(I);
+
+for J = 180:230
+    for row = 1 : size(I,1)
+        for col = 1: size(I,2)
+            if I(row,col) == J
+                I(row,col) = 0;
+            end
+        end
+    end
+end
+
+
+Se= strel('square',1);
+I = imopen(I,Se);
+I = imdilate(I,Se);
+I = imerode(I,Se);
 
 figure(2);
-imshow(T);
+imshow(I);
+I = imbinarize(I);
+I = imcomplement(I);
+figure(3);
+imshow(I);
 
